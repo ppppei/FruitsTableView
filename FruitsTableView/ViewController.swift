767d8bb2e354173eefbf8c9689aa75271b200081
ppppei <77483042+ppppei@users.noreply.github.com>
2021-09-22 23:19:57 -0700

    checking in

--- FruitsTableView/ViewController.swift
+++ FruitsTableView/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    let fruits = ["Avocado","Banana","Durian","Kiwi","Mango","Pear","Pineapple",
+               "Pomegranate","Strawberry","Watermelon"]
+    
+    @IBOutlet weak var tblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 180
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        let fruit = fruits[indexPath.row]
+        
+        cell.imgView.image = UIImage(named: fruit)
+        cell.lblImage.text = fruit
+        
+        return cell
+                        
+    }
 
 }
 
